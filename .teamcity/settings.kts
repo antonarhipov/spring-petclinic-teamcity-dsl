f05b554198c55c15c308bf5ff788ccfb70036664
Anton Arhipov <ant.arhipov@gmail.com>
2019-02-15 11:41:44 +0200

    refactoring

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.Project
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.project
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
@@ -31,52 +30,28 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2018.2"
 
 project {
-    val vcsRoot = git {
-        id("PetclinicVcs")
-        name = "PetclinicVcs"
-        url = "https://github.com/spring-projects/spring-petclinic.git"
-    }
-
-    configuration {
-        id("Build")
-        name = "Build"
-        artifactRules = "target/*jar"
-
-        vcs {
-            root(vcsRoot)
-        }
-        steps {
-            maven {
-                goals = "clean package"
-            }
-        }
-        triggers {
-            vcs { }
-        }
-    }
+    vcsRoot(PetclinicVcs)
+    buildType(Build)
 }
 
-//object Build: BuildType({
-//    name = "Build"
-//    artifactRules = "target/*jar"
-//
-//    vcs {
-//        root(PetclinicVcs)
-//    }
-//    steps {
-//        maven {
-//            goals = "clean package"
-//        }
-//    }
-//    triggers {
-//        vcs { }
-//    }
-//})
-//
-//object PetclinicVcs : GitVcsRoot({
-//    name = "PetclinicVcs"
-//    url = "https://github.com/spring-projects/spring-petclinic.git"
-//})
+object Build : BuildType({
+    name = "Build"
+    artifactRules = "target/*jar"
 
-fun Project.configuration(bt: BuildType.() -> Unit) = buildType(bt)
-fun Project.git(vcs: GitVcsRoot.()->Unit) = GitVcsRoot(vcs)
+    vcs {
+        root(PetclinicVcs)
+    }
+    steps {
+        maven {
+            goals = "clean package"
+        }
+    }
+    triggers {
+        vcs { }
+    }
+})
+
+object PetclinicVcs : GitVcsRoot({
+    name = "PetclinicVcs"
+    url = "https://github.com/spring-projects/spring-petclinic.git"
+})
